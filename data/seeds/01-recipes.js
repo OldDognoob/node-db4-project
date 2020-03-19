e97162451bf8a788b4f2a452ad42522e9d9ffa02
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-19 19:05:46 +0200

    steps-seed

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('recipes').del()
-    .then(function () {
+  return knex("recipes")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('recipes').insert([
-        {name:"Greek tzatziki"},
-        {name:"Greek Frappe"},
-        {name:"Greek Cheese Pie"}
+      return knex("recipes").insert([
+        { name: "Greek tzatziki" },
+        { name: "Greek Frappe" },
+        { name: "Greek Cheese Pie" }
       ]);
     });
 };
