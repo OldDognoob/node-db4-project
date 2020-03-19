427868b8fb0918771f04c9a2dc29262ad611e11e
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-19 19:29:00 +0200

    ingredients-seed

--- data/seeds/03-ingredients.js
+++ data/seeds/03-ingredients.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('ingredients').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('ingredients').insert([
+        {
+          name: "low fat yogurt, cucumber with pearl grated,clove garlic, pressed,tablespoons fresh lemon juice,tablespoons extra-virgin olive oil,teaspoons grated lemon zest,tablespoons chopped fresh dill,tablespoon salt,tablespoon freshly ground black pepper", 
+          quantities: "1,1/1,1,2,2,2,3,1,1"
+        },
+        {
+          name: " teaspoons NESCAFÉ® Clássico,teaspoons sugar,tablespoon cold water,ice,Evaporated Milk (optional)",
+          quantities: "2,2,1"
+        },
+        {
+          name: "sheets of phyllo pastry, feta cheese, crumbled (7 ounces), a cup milk cream,grated Parmesan or Regato or kefalograviera, grated Gouda cheese or Emedal cheese,ricotta cheese or anthotiro,large organic eggs, beaten,olive oil or melted butter,freshly ground pepper ",
+          quantities: "8-10,200gr,3/4,100gr,100gr,100gr,2,2"
+        }
       ]);
     });
 };
